ad02fbe0008d22fb594162f4f65ec0f036d665db
delchev <delchevn@gmail.com>
2024-02-11 11:20:45 +0200

    initial

---
+++ codbex-companies/gen/ui/Companies/Company/controller.js
@@ -0,0 +1,178 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-companies.Companies.Company';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/ts/codbex-companies/gen/api/Companies/CompanyService.ts";
+	}])
+	.controller('PageController', ['$scope', '$http', 'messageHub', 'entityApi', function ($scope, $http, messageHub, entityApi) {
+
+		//-----------------Custom Actions-------------------//
+		$http.get("/services/js/resources-core/services/custom-actions.js?extensionPoint=codbex-companies-custom-action").then(function (response) {
+			$scope.pageActions = response.data.filter(e => e.perspective === "Companies" && e.view === "Company" && (e.type === "page" || e.type === undefined));
+			$scope.entityActions = response.data.filter(e => e.perspective === "Companies" && e.view === "Company" && e.type === "entity");
+		});
+
+		$scope.triggerPageAction = function (actionId) {
+			for (const next of $scope.pageActions) {
+				if (next.id === actionId) {
+					messageHub.showDialogWindow("codbex-companies-custom-action", {
+						src: next.link,
+					});
+					break;
+				}
+			}
+		};
+
+		$scope.triggerEntityAction = function (actionId, selectedEntity) {
+			for (const next of $scope.entityActions) {
+				if (next.id === actionId) {
+					messageHub.showDialogWindow("codbex-companies-custom-action", {
+						src: `${next.link}?id=${selectedEntity.Id}`,
+					});
+					break;
+				}
+			}
+		};
+		//-----------------Custom Actions-------------------//
+
+		function resetPagination() {
+			$scope.dataPage = 1;
+			$scope.dataCount = 0;
+			$scope.dataLimit = 20;
+		}
+		resetPagination();
+
+		//-----------------Events-------------------//
+		messageHub.onDidReceiveMessage("entityCreated", function (msg) {
+			$scope.loadPage($scope.dataPage);
+		});
+
+		messageHub.onDidReceiveMessage("entityUpdated", function (msg) {
+			$scope.loadPage($scope.dataPage);
+		});
+		//-----------------Events-------------------//
+
+		$scope.loadPage = function (pageNumber) {
+			$scope.dataPage = pageNumber;
+			entityApi.count().then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("Company", `Unable to count Company: '${response.message}'`);
+					return;
+				}
+				$scope.dataCount = response.data;
+				let offset = (pageNumber - 1) * $scope.dataLimit;
+				let limit = $scope.dataLimit;
+				entityApi.list(offset, limit).then(function (response) {
+					if (response.status != 200) {
+						messageHub.showAlertError("Company", `Unable to list Company: '${response.message}'`);
+						return;
+					}
+					$scope.data = response.data;
+				});
+			});
+		};
+		$scope.loadPage($scope.dataPage);
+
+		$scope.selectEntity = function (entity) {
+			$scope.selectedEntity = entity;
+		};
+
+		$scope.openDetails = function (entity) {
+			$scope.selectedEntity = entity;
+			messageHub.showDialogWindow("Company-details", {
+				action: "select",
+				entity: entity,
+				optionsCity: $scope.optionsCity,
+				optionsCountry: $scope.optionsCountry,
+			});
+		};
+
+		$scope.createEntity = function () {
+			$scope.selectedEntity = null;
+			messageHub.showDialogWindow("Company-details", {
+				action: "create",
+				entity: {},
+				optionsCity: $scope.optionsCity,
+				optionsCountry: $scope.optionsCountry,
+			}, null, false);
+		};
+
+		$scope.updateEntity = function (entity) {
+			messageHub.showDialogWindow("Company-details", {
+				action: "update",
+				entity: entity,
+				optionsCity: $scope.optionsCity,
+				optionsCountry: $scope.optionsCountry,
+			}, null, false);
+		};
+
+		$scope.deleteEntity = function (entity) {
+			let id = entity.Id;
+			messageHub.showDialogAsync(
+				'Delete Company?',
+				`Are you sure you want to delete Company? This action cannot be undone.`,
+				[{
+					id: "delete-btn-yes",
+					type: "emphasized",
+					label: "Yes",
+				},
+				{
+					id: "delete-btn-no",
+					type: "normal",
+					label: "No",
+				}],
+			).then(function (msg) {
+				if (msg.data === "delete-btn-yes") {
+					entityApi.delete(id).then(function (response) {
+						if (response.status != 204) {
+							messageHub.showAlertError("Company", `Unable to delete Company: '${response.message}'`);
+							return;
+						}
+						$scope.loadPage($scope.dataPage);
+						messageHub.postMessage("clearDetails");
+					});
+				}
+			});
+		};
+
+		//----------------Dropdowns-----------------//
+		$scope.optionsCity = [];
+		$scope.optionsCountry = [];
+
+		$http.get("/services/ts/codbex-companies/gen/api/Cities/CityService.ts").then(function (response) {
+			$scope.optionsCity = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+
+		$http.get("/services/ts/codbex-companies/gen/api/Countries/CountryService.ts").then(function (response) {
+			$scope.optionsCountry = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+		$scope.optionsCityValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsCity.length; i++) {
+				if ($scope.optionsCity[i].value === optionKey) {
+					return $scope.optionsCity[i].text;
+				}
+			}
+			return null;
+		};
+		$scope.optionsCountryValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsCountry.length; i++) {
+				if ($scope.optionsCountry[i].value === optionKey) {
+					return $scope.optionsCountry[i].text;
+				}
+			}
+			return null;
+		};
+		//----------------Dropdowns-----------------//
+
+	}]);
