193a1c85a10380fa3b3535bdf50d61dc1a789f19
NicoleNG18 <nicole.georgieva18@abv.bg>
2024-09-05 14:23:41 +0300

    remove organization from company

--- codbex-companies/gen/codbex-companies/api/Companies/CompanyService.ts
+++ codbex-companies/gen/codbex-companies/api/Companies/CompanyService.ts
@@ -149,12 +149,6 @@ class CompanyService {
         if (entity.PostCode?.length > 20) {
             throw new ValidationError(`The 'PostCode' exceeds the maximum length of [20] characters`);
         }
-        if (entity.Country === null || entity.Country === undefined) {
-            throw new ValidationError(`The 'Country' property is required, provide a valid value`);
-        }
-        if (entity.City === null || entity.City === undefined) {
-            throw new ValidationError(`The 'City' property is required, provide a valid value`);
-        }
         if (entity.TIN?.length > 20) {
             throw new ValidationError(`The 'TIN' exceeds the maximum length of [20] characters`);
         }
