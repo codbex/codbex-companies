f85207d8f49a5c69abb0a2f1c9c6a54b765ea7d5
ThuF <jordan.p.pavlov@gmail.com>
2026-03-23 15:22:38 +0000

    Migrate from project.json to package.json

--- codbex-companies/gen/codbex-companies/api/Companies/CompanyController.ts
+++ codbex-companies/gen/codbex-companies/api/Companies/CompanyController.ts
@@ -44,7 +44,7 @@ class CompanyController {
             this.checkPermissions('write');
             this.validateEntity(entity);
             entity.Id = this.repository.create(entity) as any;
-            response.setHeader('Content-Location', '/services/ts/codbex-companies/gen/codbex-companies/api/Companies/CompanyService.ts/' + entity.Id);
+            response.setHeader('Content-Location', '/services/ts/codbex-companies/gen/codbex-companies/api/Companies/CompanyController.ts/' + entity.Id);
             response.setStatus(response.CREATED);
             return entity;
         } catch (error: any) {
