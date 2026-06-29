7311c504e0b1181e016baa81c14af50ef7061f64
NicoleNG18 <nicole.georgieva18@abv.bg>
2026-06-29 16:08:45 +0300

    regenerate module with the latest template

--- codbex-companies/gen/codbex_companies/data/companies/CompanyRepository.java
+++ codbex-companies/gen/codbex_companies/data/companies/CompanyRepository.java
@@ -1,7 +1,7 @@
 package gen.codbex_companies.data.companies;
 
 import org.eclipse.dirigible.components.data.store.java.repository.JavaRepository;
-import org.eclipse.dirigible.engine.java.annotations.Repository;
+import org.eclipse.dirigible.sdk.component.Repository;
 
 @Repository
 public class CompanyRepository extends JavaRepository<CompanyEntity> {
